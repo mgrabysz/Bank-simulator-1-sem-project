226f651cb42b8373639201665151cc24cb55ef97
Marcin Grabysz <margrabysz@gmail.com>
2020-12-29 18:58:03 +0100

    Implemented classes Bank, Loan, Client

---
+++ bank_classes.py
@@ -0,0 +1,362 @@
+from decimal import Decimal
+
+
+class InvalidValueError(Exception):
+    pass
+
+
+class InvalidRateError(Exception):
+    pass
+
+
+class InvalidInstallmentsError(Exception):
+    pass
+
+
+class InvalidNameError(Exception):
+    pass
+
+
+def value_is_correct(value):
+    """
+    Returns True if given value is a positive number
+    """
+    try:
+        value = float(value)
+        if value > 0:
+            return True
+        else:
+            raise InvalidValueError
+    except ValueError:
+        raise InvalidValueError
+
+
+def rate_is_correct(rate):
+    """
+    Returns True if given rate is a number from range [0, 100]
+    """
+    try:
+        rate = float(rate)
+        if rate < 0 or rate > 100:
+            raise InvalidRateError
+        else:
+            return True
+    except ValueError:
+        raise InvalidRateError
+
+
+def installments_is_correct(installments):
+    """
+    Returns True if given installments is positive integer
+    """
+    if type(installments) is not int:
+        raise InvalidInstallmentsError
+    if installments < 1:
+        raise InvalidInstallmentsError
+    return True
+
+
+class Bank():
+    """
+    Class Bank. Contains attributes:
+    :param budget: bank's total budget
+    :type budget: Decimal
+
+    :param clients_loans: dictionary mapping client to his loans
+    :type clients_loans: dict
+
+    :param clients_id: dictionary mapping unique ID to client
+    :type clients_id: dict
+
+    :param id_count: this param increases by 1 with every created client
+        to provide uniqueness of ID
+    :type id_count: int
+    """
+    def __init__(self, budget=Decimal(1000000)):
+        self._budget = budget
+        self._clients_loans = {}
+        self._clients_id = {}
+        self.id_count = 1
+
+    def budget(self):
+        return self._budget
+
+    def increase_budget(self, value):
+        """
+        1. Increases budget by given value
+        2. Rounds budget to have monetary format
+        """
+        value = str(value)
+        self._budget += Decimal(value)
+        var = self._budget
+        self._budget = self._budget.quantize(Decimal('.01'))
+        var.quantize(Decimal('.01'))
+        pass
+
+    def decrease_budget(self, value):
+        """
+        1. Decreases budget by given value
+        2. Rounds budget to have monetary format
+        """
+        value = str(value)
+        self._budget -= Decimal(value)
+        self._budget = self._budget.quantize(Decimal('.01'))
+        pass
+
+    def clients_loans(self):
+        return self._clients_loans
+
+    def clients_id(self):
+        return self._clients_id
+
+    def add_new_client(self, client_name):
+        """
+        1. Creates unique id for client
+        2. Appends new elements to clients_id and clients_loans
+        3. Increases id_count by 1
+        """
+        id = self.id_count
+        client = Client(client_name, id)
+        self._clients_id[id] = client
+        self._clients_loans[client] = []
+        self.id_count += 1
+        return id
+
+    def give_loan_to_bank_client(self, client_id, value, rate, installments):
+        """
+        1. Creates new loan assigned to a client, who already has a loan.
+        2. Decreases bank budget by loan value
+        """
+        client = self.clients_id()[client_id]
+        loan = Loan(value, rate, installments)
+        self._clients_loans[client].append(loan)
+        self.decrease_budget(value)
+
+    def give_loan_to_new_client(self, client_name, value, rate, installments):
+        """
+        1. Creates new client and then creates new loan assigned to this client
+        2. Decreases bank budet by loan value
+        """
+        client_id = self.add_new_client(client_name)
+        self.give_loan_to_bank_client(client_id, value, rate, installments)
+
+    def collect_payment(self, loan):
+        """
+        1. Increases bank budget by single payment
+        2. Decreases loan.installments by 1
+        3. If loan.installments == 0, returns False. Otherwise returns True
+        """
+        payment = loan.payment()
+        self.increase_budget(payment)
+        loan.decrease_installments_by_one()
+        to_return = False if loan.installments() == 0 else True
+        return to_return
+
+    def collect_all_payments(self):
+        """
+        1. Iterates over clients_loans dictionary
+        2. Collect payments from every loan of every client
+        3. If function collect_payment returns False, discards loan
+        4. If client's only loan is discarded, discard client
+        """
+        clients_to_discard = []
+        for client, loan_list in self.clients_loans().items():
+            loans_to_discard = []
+            for loan in loan_list:
+                loan_is_valid = self.collect_payment(loan)
+                if not loan_is_valid:
+                    loans_to_discard.append(loan)
+            for loan in loans_to_discard:
+                loan_list.remove(loan)
+            if not loan_list:
+                clients_to_discard.append(client)
+
+    # This bizarre construct is meant to prevent removing
+    # list/dict element during iteration over that list/dict
+
+        for client in clients_to_discard:
+            del self._clients_loans[client]
+
+    def client_debt(self, client):
+        """
+        Calcultes how much a client owes to the bank
+        """
+        debt = Decimal(0)
+        loans = self.clients_loans()[client]
+        for loan in loans:
+            installments = loan.installments()
+            payment = loan.payment()
+            to_pay = installments * payment
+            debt += to_pay
+        return debt
+
+    def info_about_clients(self):
+        """
+        Returns dictionary according to scheme:
+        {client_id: {name: name, debt: debt}}
+        """
+        info_about_clients = {}
+        for client in self.clients_loans():
+            name = client.name()
+            id = client.id()
+            debt = self.client_debt(client)
+            name_debt = {"name": name, "debt": debt}
+            info_about_clients[id] = name_debt
+        return info_about_clients
+
+    def info_about_clients_to_print(self):
+        """
+        Returns a string containing info about clients:
+            - id
+            - name
+            - debt
+        """
+        info = self.info_about_clients()
+        to_return = 'ID - name - debt\n'
+        for id, values in info.items():
+            name = values['name']
+            debt = values['debt']
+            to_return += f'{id} - {name} - {debt} zł\n'
+        return to_return
+
+    # def info_about_single_client(self, client):
+    #     """
+    #     Returns a string containing:
+    #         - client's ID
+    #         - client's name
+    #         - client's loans and its specification
+    #     """
+    #     id = client.id()
+    #     name = client.name()
+    #     loans = self.clients_loans()[client]
+    #     debt = bank.client_debt(client)
+    #     to_return = f'NAME: {name} ID: {id}\nTOTAL DEBT: {debt}\nLOANS:\n'
+    #     for loan in enumerate(loans):
+    #         rate = loan.rate()
+    #         installments = loan.installments()
+    #         payment = loan.payment
+    #         to_pay = payment * installments
+
+    def info_about_single_client(self, client):
+        """
+        Returns a dictionary with info about client according to scheme:
+        {
+            id: id,
+            name: name,
+            total debt: debt
+            loans info: {
+                1: {value: a, rate: b, installments: c, payment: d, to pay: e}
+                2: {value: a, rate: b, installments: c, payment: d, to pay: e}
+                    }
+         }
+        """
+        id = client.id()
+        name = client.name()
+        total_debt = self.client_debt(client)
+        single_client_info = {
+            "id": id,
+            "name": name,
+            "total debt": total_debt,
+            "loans info": {}
+        }
+        loans = self.clients_loans()[client]
+        for index, loan in enumerate(loans):
+            value = loan.value()
+            rate = loan.rate()
+            installments = loan.installments()
+            payment = loan.payment()
+            to_pay = installments * payment
+
+            loan_info = {
+                'value': value,
+                'rate': rate,
+                'installments': installments,
+                'payment': payment,
+                'left to pay': to_pay
+            }
+            single_client_info["loans info"][index + 1] = loan_info
+        return single_client_info
+
+
+class Loan():
+    """
+    Class Loan. Contains attributes:
+
+    :param value: total value of a loan
+    :type value: Decimal
+
+    :param rate: rate of interest
+    :type rate: Decimal
+
+    :param installments: number of installments left
+    :type installments: int
+
+    :param payment: value of single payment
+    :type: Decimal
+    """
+    def __init__(self,
+                 value,
+                 rate,
+                 installments,
+                 ):
+
+        if value_is_correct(value):
+            self._value = Decimal(str(value))
+
+        if rate_is_correct(rate):
+            self._rate = Decimal(str(rate))
+
+        if installments_is_correct(installments):
+            self._installments = installments
+
+        self._payment = self.calculate_payment()
+
+    def calculate_payment(self):
+        """
+        Calculates value of a single installment
+        """
+        rate = self.rate()
+        value = self.value()
+        installments = self.installments()
+
+        payment = (((rate + 100) * value / 100) / installments)
+        return payment.quantize(Decimal('.01'))
+
+    def value(self):
+        return self._value
+
+    def rate(self):
+        return self._rate
+
+    def installments(self):
+        return self._installments
+
+    def set_installments(self, new_installments):
+        self._installments = new_installments
+
+    def decrease_installments_by_one(self):
+        self._installments -= 1
+
+    def payment(self):
+        return self._payment
+
+
+class Client():
+    """
+    :param name: client's name
+    :type name: str
+
+    :param id: client's unique ID
+    :type id: int
+    """
+    def __init__(self, name, id):
+        self._name = str(name)
+        if self._name == '':
+            raise InvalidNameError
+        self._id = id
+
+    def name(self):
+        return self._name
+
+    def id(self):
+        return self._id
---
+++ test_bank_classes.py
@@ -0,0 +1,362 @@
+from bank_classes import (
+    Bank,
+    Loan,
+    Client,
+    InvalidValueError,
+    InvalidInstallmentsError,
+    InvalidRateError,
+    InvalidNameError,
+    value_is_correct,
+    rate_is_correct,
+    installments_is_correct,
+)
+from decimal import Decimal
+import pytest
+
+
+def test_create_bank():
+    bank = Bank()
+    assert bank.budget() == Decimal('1000000')
+    assert bank.clients_loans() == {}
+    assert bank.clients_id() == {}
+    assert bank.id_count == 1
+
+
+def test_increase_budget_integer():
+    bank = Bank()
+    bank.increase_budget(250)
+    assert bank.budget() == Decimal('1000250')
+
+
+def test_increase_budget_non_integer():
+    bank = Bank()
+    bank.increase_budget(20.2)
+    assert bank.budget() == Decimal('1000020.20')
+
+
+def test_increase_budget_and_round():
+    bank = Bank()
+    bank.increase_budget(20.23778)
+    assert bank.budget() == Decimal('1000020.24')
+
+
+def test_decrease_budget_integer():
+    bank = Bank()
+    bank.decrease_budget(300)
+    assert bank.budget() == Decimal('999700')
+
+
+def test_decrease_budget_non_integer():
+    bank = Bank()
+    bank.decrease_budget(300.2)
+    assert bank.budget() == Decimal('999699.80')
+
+
+def test_decrease_budget_and_round():
+    bank = Bank()
+    bank.decrease_budget(20.3434)
+    assert bank.budget() == Decimal('999979.66')
+
+
+def test_value_is_correct():
+    assert value_is_correct(332.46) is True
+
+
+def test_value_is_correct_negative():
+    with pytest.raises(InvalidValueError):
+        value_is_correct(-22.9)
+
+
+def test_value_is_correct_string():
+    with pytest.raises(InvalidValueError):
+        value_is_correct('Little Mermaid')
+
+
+def test_rate_is_correct():
+    assert rate_is_correct(55.6) is True
+
+
+def test_rate_is_correct_negative():
+    with pytest.raises(InvalidRateError):
+        rate_is_correct(-88)
+
+
+def test_rate_is_correct_greater_than_100():
+    with pytest.raises(InvalidRateError):
+        rate_is_correct(732.66)
+
+
+def test_rate_is_correct_string():
+    with pytest.raises(InvalidRateError):
+        rate_is_correct('Little Mermaid')
+
+
+def test_installments_is_correct():
+    assert installments_is_correct(12) is True
+
+
+def test_installments_is_correct_non_integer():
+    with pytest.raises(InvalidInstallmentsError):
+        installments_is_correct(9.2)
+
+
+def test_installments_is_correct_negative():
+    with pytest.raises(InvalidInstallmentsError):
+        installments_is_correct(-9)
+
+
+def test_installments_is_correct_string():
+    with pytest.raises(InvalidInstallmentsError):
+        installments_is_correct('Little Mermaid')
+
+
+def test_create_loan():
+    loan = Loan(1000, 2, 10)
+    assert loan.value() == Decimal(1000)
+    assert loan.rate() == Decimal(2)
+    assert loan.installments() == 10
+    assert loan.payment() == Decimal(102)
+
+
+def test_create_loan_non_integers():
+    loan = Loan(1005.4, 2.2, 12)
+    assert loan.value() == Decimal('1005.4')
+    assert loan.rate() == Decimal('2.2')
+    assert loan.installments() == 12
+    assert loan.payment() == Decimal('85.63')
+
+
+def test_create_client():
+    client = Client('Jose Arcadio Morales', 2)
+    assert client.name() == 'Jose Arcadio Morales'
+    assert client.id() == 2
+
+
+def test_create_client_with_empty_name():
+    with pytest.raises(InvalidNameError):
+        Client('', 3)
+
+
+def test_bank_add_new_client():
+    bank = Bank()
+    assert bank.clients_id() == {}
+    bank.add_new_client('Jose Arcadio Morales')
+    client = bank.clients_id()[1]
+    assert client.name() == 'Jose Arcadio Morales'
+    assert bank.clients_loans()[client] == []
+
+
+def test_add_new_client_twice():
+    bank = Bank()
+    bank.add_new_client('Jose Arcadio Morales')
+    bank.add_new_client('Emma Watson')
+    client = bank.clients_id()[2]
+    assert client.name() == 'Emma Watson'
+    assert bank.clients_loans()[client] == []
+
+
+def test_add_new_client_twice_same_name():
+    bank = Bank()
+    bank.add_new_client('Carlos')
+    bank.add_new_client('Carlos')
+    first_client = bank.clients_id()[1]
+    second_client = bank.clients_id()[2]
+    assert first_client.name() == 'Carlos'
+    assert first_client.id() == 1
+    assert second_client.name() == 'Carlos'
+    assert second_client.id() == 2
+
+
+def test_add_new_client_invalid_name():
+    bank = Bank()
+    with pytest.raises(InvalidNameError):
+        bank.add_new_client('')
+
+
+def test_give_loan_to_bank_client():
+    bank = Bank()
+    bank.add_new_client('Jose Arcadio Morales')
+    client = bank.clients_id()[1]
+    bank.give_loan_to_bank_client(1, 1000, 4, 10)
+    loans = bank.clients_loans()[client]
+    loan = loans[0]
+    assert loan.value() == 1000
+    assert loan.rate() == 4
+    assert loan.installments() == 10
+    assert bank.budget() == Decimal('999000')
+
+
+def test_give_loan_to_new_client():
+    bank = Bank()
+    bank.give_loan_to_new_client('Emma Watson', 1200, 3, 12)
+    client = bank.clients_id()[1]
+    loans = loans = bank.clients_loans()[client]
+    loan = loans[0]
+    assert client.name() == 'Emma Watson'
+    assert loan.value() == 1200
+    assert loan.rate() == 3
+    assert loan.installments() == 12
+    assert bank.budget() == Decimal('998800')
+
+
+def test_give_two_loans_to_new_client():
+    bank = Bank()
+    bank.give_loan_to_new_client('Emma Watson', 1200, 3, 12)
+    bank.give_loan_to_bank_client(1, 500, 2, 4)
+    client = bank.clients_id()[1]
+    loans = bank.clients_loans()[client]
+    first_loan = loans[0]
+    second_loan = loans[1]
+    assert first_loan.value() == 1200
+    assert first_loan.rate() == 3
+    assert first_loan.installments() == 12
+    assert second_loan.value() == 500
+    assert second_loan.rate() == 2
+    assert second_loan.installments() == 4
+    assert bank.budget() == Decimal('998300')
+
+
+def test_collect_payment():
+    bank = Bank()
+    bank.give_loan_to_new_client('Emma Watson', 1200, 3, 12)
+    client = bank.clients_id()[1]
+    loans = bank.clients_loans()[client]
+    loan = loans[0]
+
+    assert bank.budget() == Decimal('998800')
+    assert loan.payment() == 103
+    assert loan.installments() == 12
+
+    assert bank.collect_payment(loan) is True
+
+    assert bank.budget() == Decimal('998903')
+    assert loan.installments() == 11
+
+
+def test_collect_last_payment():
+    bank = Bank()
+    bank.give_loan_to_new_client('Emma Watson', 1200, 3, 12)
+    client = bank.clients_id()[1]
+    loans = bank.clients_loans()[client]
+    loan = loans[0]
+
+    for _ in range(11):
+        bank.collect_payment(loan)
+
+    assert bank.collect_payment(loan) is False
+    assert bank.budget() == Decimal('1000036')
+
+
+def test_collect_all_payments():
+    bank = Bank()
+    bank.give_loan_to_new_client('Emma Watson', 1200, 3, 12)
+    bank.give_loan_to_new_client('Jose Arcadio Morales', 1000, 3, 1)
+    bank.give_loan_to_bank_client(1, 500, 5, 1)
+    emma = bank.clients_id()[1]
+    jose = bank.clients_id()[2]
+    assert bank.budget() == Decimal('997300')
+    bank.collect_all_payments()
+    assert bank.budget() == Decimal('998958')
+    assert emma in bank.clients_loans()
+    assert jose not in bank.clients_loans()
+    emma_loans = bank.clients_loans()[emma]
+    assert len(emma_loans) == 1
+
+
+def test_collect_all_payments_multiple_times():
+    bank = Bank()
+    bank.give_loan_to_new_client('Emma Watson', 1200, 3, 1)
+    bank.give_loan_to_bank_client(1, 500, 5, 2)
+    bank.give_loan_to_bank_client(1, 300, 1, 3)
+    emma = bank.clients_id()[1]
+    assert bank.budget() == Decimal('998000')
+    assert len(bank.clients_loans()[emma]) == 3
+
+    bank.collect_all_payments()
+    assert bank.budget() == Decimal('999599.5')
+    assert len(bank.clients_loans()[emma]) == 2
+
+    bank.collect_all_payments()
+    assert bank.budget() == Decimal('999963')
+    assert len(bank.clients_loans()[emma]) == 1
+
+    bank.collect_all_payments()
+    assert bank.budget() == Decimal('1000064')
+    assert emma not in bank.clients_loans()
+
+    bank.collect_all_payments()
+    assert bank.budget() == Decimal('1000064')
+
+
+def test_client_debt():
+    bank = Bank()
+    bank.give_loan_to_new_client('Emma Watson', 1200, 3, 1)
+    bank.give_loan_to_bank_client(1, 500, 5, 2)
+    bank.give_loan_to_bank_client(1, 300, 1, 3)
+    emma = bank.clients_id()[1]
+    debt = bank.client_debt(emma)
+    assert debt == Decimal('2064')
+
+    bank.collect_all_payments()
+    new_debt = bank.client_debt(emma)
+    assert new_debt == Decimal('464.50')
+
+    bank.collect_all_payments()
+    newest_debt = bank.client_debt(emma)
+    assert newest_debt == Decimal('101')
+
+
+def test_info_about_clients():
+    bank = Bank()
+    bank.give_loan_to_new_client('Emma Watson', 1200, 3, 12)
+    bank.give_loan_to_new_client('Jose Arcadio Morales', 1000, 3, 1)
+    bank.give_loan_to_bank_client(1, 500, 5, 1)
+    info = bank.info_about_clients()
+    assert info == {
+        1: {'name': 'Emma Watson', 'debt': Decimal('1761.00')},
+        2: {'name': 'Jose Arcadio Morales', 'debt': Decimal('1030.00')}
+    }
+
+
+def test_info_about_clients_to_print():
+    bank = Bank()
+    bank.give_loan_to_new_client('Emma Watson', 1200, 3, 12)
+    bank.give_loan_to_new_client('Jose Arcadio Morales', 1000, 3, 1)
+    bank.give_loan_to_bank_client(1, 500, 5, 1)
+    to_print = bank.info_about_clients_to_print()
+
+    assert to_print == ('ID - name - debt\n' +
+                        '1 - Emma Watson - 1761.00 zł\n' +
+                        '2 - Jose Arcadio Morales - 1030.00 zł\n'
+                        )
+
+
+def test_info_about_single_client():
+    bank = Bank()
+    bank.give_loan_to_new_client('Emma Watson', 1200, 3, 12)
+    bank.give_loan_to_bank_client(1, 500, 5, 2)
+    emma = bank.clients_id()[1]
+    info = bank.info_about_single_client(emma)
+
+    desired_info = {
+        'id': 1,
+        'name': 'Emma Watson',
+        'total debt': Decimal('1761.00'),
+        'loans info': {
+            1: {
+                'value': 1200,
+                'rate': 3,
+                'installments': 12,
+                'payment': 103,
+                'left to pay': 1236
+            },
+            2: {
+                'value': 500,
+                'rate': 5,
+                'installments': 2,
+                'payment': Decimal('262.50'),
+                'left to pay': 525
+            }
+        }
+    }
+    assert info == desired_info
